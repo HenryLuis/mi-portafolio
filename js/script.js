7cce0ce1bc08af377402fb568bd58755a83fd87c
HenryLuis <hlmo10@gmail.com>
2025-11-28 12:38:47 -0400

    Agregamos animaiones y evento de bienvenida

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 console.log('Bienvenido');
-alert('Hola...Gracias por visitarme');
+//alert('Hola...Gracias por visitarme');
+document.querySelector('h1').addEventListener('click',()=>{
+    alert('Hola...Gracias por visitarme');
+});
+
